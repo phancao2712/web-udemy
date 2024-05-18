640e811ca2c96e22e0e2ff03d13c5e6496051732
phancao2712 <phancaoa1@gmail.com>
2024-05-18 08:25:29 +0700

    setup modules

---
+++ Modules/ModuleServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
